069de69402753365536be678c7f8cacf84738136
cgrepo <genesis62@hotmail.com>
2023-04-19 00:01:37 -0700

    working on form search by curp

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -6,7 +6,14 @@ class PersonController < ApplicationController
   def fisica_find; end
 
   def find_curp
-  debugger
+    dataset = PersonaFisica.by_curp(params[:curp_mf3])
+    if dataset.count == 1
+      @persona = dataset.first
+    elsif dataset.count > 1
+      @personas = dataset.all
+    else
+      @personas = nil
+    end
   end
   
   def moral; end
